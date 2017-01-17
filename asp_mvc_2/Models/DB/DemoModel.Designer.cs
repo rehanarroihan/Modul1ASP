35bef225bb08eaa48913c7ea99d9891068d5f8a8
rehanarroihan <third2014project@gmail.com>
2017-01-17 12:21:53 +0700

    Add project files.

---
+++ asp_mvc_2/Models/DB/DemoModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'F:\PELAJARAN\dekstop\asp_mvc_2\asp_mvc_2\Models\DB\DemoModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
